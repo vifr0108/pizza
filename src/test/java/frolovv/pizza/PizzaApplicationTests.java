4c3eb4cdfc94449bd841b5c564e9eae46f7a1c8d
Frolov Vitaly, INI-DOS-O2A (EXT) <Vitaly.Frolov@swisscom.com>
2020-10-08 12:59:16 +0400

    UniTestForShowcase

--- src/test/java/frolovv/pizza/PizzaApplicationTests.java
+++ src/test/java/frolovv/pizza/PizzaApplicationTests.java
@@ -4,26 +4,16 @@ import frolovv.pizza.entity.Showcase;
 import frolovv.pizza.repository.ShowcaseRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
-import javax.persistence.EntityManager;
 import java.util.stream.Stream;
 
 @Slf4j
+@SpringBootTest(classes = PizzaApplication.class)
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-class PizzaApplicationTests {
-
-	@Autowired
-	EntityManager entityManager;
-
+public class PizzaApplicationTests {
 	@Autowired
 	ShowcaseRepository showcaseRepository;
 
@@ -32,23 +22,17 @@ class PizzaApplicationTests {
 	{
 		Stream.of("Felix", "Garfield", "Whiskers").forEach(
 				n -> showcaseRepository.save(new Showcase(n)));
-
-		Showcase sc = new Showcase("Mark");
-		sc.setId(1L);
-		sc.setName("Margarita");
-		sc.setNumber(1);
-		sc.setCapacity(1);
-		showcaseRepository.save(sc);
 	}
 
-	@Test
-	void contextLoads() {
+	@org.junit.Test
+	public void testShowcaseRepositoryFindAll() {
 		log.error("******Start test testFindAll*****");
 		Iterable<Showcase> allShowcase = showcaseRepository.findAll();
 		for (Showcase showcase : allShowcase)
 		{
 			log.error(showcase.getName());
 		}
+		assert (showcaseRepository.count() == 3);
 		log.error("******End test testFindAll*****");
 	}
 
