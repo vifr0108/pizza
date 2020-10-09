b9dc34d091e83ac730834d8b1d12ee6d07aeecbf
Frolov Vitaly, INI-DOS-O2A (EXT) <Vitaly.Frolov@swisscom.com>
2020-10-09 13:31:31 +0400

    add rest for showcase

--- src/main/java/frolovv/pizza/repository/ShowcaseRepository.java
+++ src/main/java/frolovv/pizza/repository/ShowcaseRepository.java
@@ -2,8 +2,13 @@ package frolovv.pizza.repository;
 
 import frolovv.pizza.entity.Showcase;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
+@RepositoryRestResource(collectionResourceRel = "pizzatable", path = "pizzatable")
 public interface ShowcaseRepository extends JpaRepository<Showcase, Long>
 {
-
+    List<Showcase> findByName(@Param("name") String name);
 }
